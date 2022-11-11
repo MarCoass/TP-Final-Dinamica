3defe2d42762c5b5abd1cdbef64f4caab98dc834
patito <micaela.paillamilla@est.fi.uncoma.edu.ar>
2022-11-11 20:39:19 -0300

    Avances en login

---
+++ Vista/Assets/Js/login.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+    document.getElementById('nuevo_usuario').style.display = 'none';
+});
+function mostrar_nuevo_usuario(){
+    document.getElementById('login_from').style.display = 'none';
+    document.getElementById('nuevo_usuario').style.display = 'block';
+}
+
+function mostrar_login(){
+    document.getElementById('nuevo_usuario').style.display = 'none';
+    document.getElementById('login_from').style.display = 'block';
+}
+
+function registrar_nuevo_usuario(){
+    var usuario = md5($('#usuario_nuevo').val());
+
+    console(usuario);
+}
