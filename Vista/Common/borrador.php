ffb39449c10c8164cce3d14285c1aa8ffaded4ad
MartinaRosales <martina.est.fi.uncoma.edu.ar>
2022-11-18 15:31:40 -0300

    Los elementos que saqué del header

---
+++ Vista/Common/borrador.php
@@ -0,0 +1,18 @@
+<div class="collapse navbar-collapse" id="navbarHeader">
+<ul class="navbar-nav">
+    <li class="nav-item">
+        <a class="nav-link active text-light fs-5" aria-current="page" href="/TP-Final-Dinamica/Vista/Home.php">Inicio</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link text-light fs-5" href="#">Informes</a>
+    </li>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle text-light fs-5" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Productos
+        </a>
+        <ul class="dropdown-menu" id="dropdown">
+            <li><a class="dropdown-item text-light" href="/TP-Final-Dinamica/Vista/Productos2D.php">Impresiones 2D</a></li>
+            <li><a class="dropdown-item text-light" href="/TP-Final-Dinamica/Vista/Productos3D.php">Impresiones 3D</a></li>
+            <li><a class="dropdown-item text-light" href="/TP-Final-Dinamica/Vista/Accesorios.php">Accesorios</a></li>
+        </ul>
+    </li>
