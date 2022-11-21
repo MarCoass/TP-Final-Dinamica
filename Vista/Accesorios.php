a44f5e952c21ac13b581e3ab3c7343dda2fa8434
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2022-11-21 16:22:52 -0300

    modifique como se ven las card de accesorios

--- Vista/Accesorios.php
+++ Vista/Accesorios.php
@@ -1,9 +1,9 @@
 <?php
- 
+
 include_once("Common/Header.php");
 
 $objControlProducto = new C_Producto();
-$param =['protipo' => "Accesorio"];
+$param = ['protipo' => "Accesorio"];
 $arrayProductos = $objControlProducto->buscar($param);
 if ($arrayProductos != null) {
     $cantidadProductos = count($arrayProductos);
@@ -12,23 +12,24 @@ if ($arrayProductos != null) {
 }
 $i = 0;
 ?>
-<div class="container col-md-5 mt-3 ms-3">
-    <div>
-        <div>
+<div class="container-fluid  mt-3" style="width: 100vw;">
+    <div class="row ">
+
         <?php
         while ($i < $cantidadProductos) {
         ?>
-            <div>
+            <div class="col-4">
                 <div class="card mb-3" style="max-width: 540px;">
                     <div class="row g-0">
                         <div class="col-md-4">
-                        <img src="<?php echo $arrayProductos[$i]->getProimagen() ?>" class="img-fluid rounded-start" alt="Foto del producto">
+                            <img src="<?php echo $arrayProductos[$i]->getProimagen() ?>" class="img-fluid rounded-start" alt="Foto del producto">
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
                                 <h5 class="card-title"><?php echo $arrayProductos[$i]->getPronombre() ?></h5>
                                 <p class="card-text"><b>Descripci&oacute;n:</b> <?php echo $arrayProductos[$i]->getProdetalle() ?> <br>
-                                                     <b>Precio:</b>  $<?php echo $arrayProductos[$i]->getProprecio() ?></p>
+                                    <b>Precio:</b> $<?php echo $arrayProductos[$i]->getProprecio() ?>
+                                </p>
                             </div>
                             <div class="card-footer">
                                 <button class="btn  btn-outline-dark"><i class="bi bi-cart-plus-fill"></i> Añadir al carrito</button>
@@ -37,13 +38,13 @@ $i = 0;
                     </div>
                 </div>
             </div>
-            <?php
-                $i++;
-            }
-            ?>
-        </div>
+        <?php
+            $i++;
+        }
+        ?>
     </div>
 </div>
+
 
 
 <?php
