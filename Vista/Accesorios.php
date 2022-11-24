5bf4b30ac5b441cadbe6990ea35ae14bf2308411
patito <micaela.paillamilla@est.fi.uncoma.edu.ar>
2022-11-24 16:15:13 -0300

    Mejoras

--- Vista/Accesorios.php
+++ Vista/Accesorios.php
@@ -12,41 +12,34 @@ if ($arrayProductos != null) {
 }
 $i = 0;
 ?>
-<div class="container-fluid  mt-3 ms-3" >
-    <div class="row ">
-
-        <?php
-        while ($i < $cantidadProductos) {
+<div class="container mt-5">
+<div class="row" style="justify-content: center;">
+<?php
+while ($i < $cantidadProductos) {
         ?>
-            <div class="col-4">
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="<?php echo $arrayProductos[$i]->getProimagen() ?>" class="img-fluid rounded-start" alt="Foto del producto">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title"><?php echo $arrayProductos[$i]->getPronombre() ?></h5>
-                                <p class="card-text"><b>Descripci&oacute;n:</b> <?php echo $arrayProductos[$i]->getProdetalle() ?> <br>
-                                    <b>Precio:</b> $<?php echo $arrayProductos[$i]->getProprecio() ?>
-                                </p>
-                            </div>
-                            <div class="card-footer">
-                                <button class="btn  btn-outline-dark"><i class="bi bi-cart-plus-fill"></i> Añadir al carrito</button>
-                            </div>
-                        </div>
-                    </div>
+<div class="card m-4" style="width: 18rem;">
+        <form id="formulario" name="formulario" method="post" action="cart.php">
+        <input name="precio" type="hidden" id="precio" value="<?php echo $arrayProductos[$i]->getProprecio();?>" />
+        <input name="titulo" type="hidden" id="titulo" value="<?php echo $arrayProductos[$i]->getPronombre();?>" />
+        <input name="cantidad" type="hidden" id="cantidad" value="1" class="pl-2" />
+        <img src="<?php echo $arrayProductos[$i]->getProimagen() ?>" class="card-img-top" alt="Foto del producto">
+                       
+                <div class="card-body">
+                        <h5 class="card-title"><?php echo $arrayProductos[$i]->getPronombre() ?></h5>
+                        <p class="card-text"><b>Descripci&oacute;n:</b> <?php echo $arrayProductos[$i]->getProdetalle() ?> - Precio:  $<?php echo $arrayProductos[$i]->getProprecio() ?></p>
+                        
                 </div>
-            </div>
-        <?php
-            $i++;
-        }
-        ?>
-    </div>
+                <div class="card-footer">
+                        <button class="btn  btn-outline-dark" type="submit"><i class="bi bi-cart-plus-fill"></i> Añadir al carrito</button>
+                </div>
+        </form>
 </div>
 
-
-
+<?php
+    $i++;
+}?>
+</div>
+</div>
 <?php
 include_once("Common/Footer.php");
 ?>
