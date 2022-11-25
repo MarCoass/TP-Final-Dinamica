e39eede48a59b227a1dee8f56627fabfcfc36107
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2022-11-25 17:38:28 -0300

    help

--- Vista/Assets/Js/cambiarEstadoCompra.js
+++ Vista/Assets/Js/cambiarEstadoCompra.js
@@ -1,13 +1,16 @@
 
-$(document).on('click', '.cambiarEstado', function() {
-    const form = $(this.parentNode);
-    console.log(form);
+
+function enviarDato(id_compra){
     $.ajax({
         type: "POST",
         url: 'Accion/cambiarEstado.php',
-        data: form.serialize(),
+        data: {
+            idcompraestado: $('#idcompraestado_'+id_compra).val(),
+            idcompra: id_compra,
+            idcompraestadotipo: $('#idcompraestadotipo_'+id_compra).val()
+        },
         success: function(response){
             location.reload();
        }
    });
-});
+}
