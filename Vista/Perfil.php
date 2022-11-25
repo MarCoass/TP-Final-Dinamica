b9a63a514f178be2b37da013b0befe83d76527d1
MartinaRosales <martina.est.fi.uncoma.edu.ar>
2022-11-25 17:44:07 -0300

    cambios

--- Vista/Perfil.php
+++ Vista/Perfil.php
@@ -16,8 +16,8 @@ $usuario = $sesion->getUsuario();
         Contraseña: ***********
       </div>
       <div id="ContenedorBotones m-5">
-        <a class="nav-link" data-bs-toggle="modal" data-bs-target="#modal_editar"><button class="btn btn-light">Editar</button></a>
-        <a class="nav-link" data-bs-toggle="modal" data-bs-target="#modal_borrar"><button class="btn btn-danger">Borrar cuenta</button></a>
+        <a class="nav-link" data-bs-toggle="modal" data-bs-target="#modal_editar"><button class="btn btn-outline-light mt-2">Editar</button></a>
+        <a class="nav-link" data-bs-toggle="modal" data-bs-target="#modal_borrar"><button class="btn btn-danger mt-2">Borrar cuenta</button></a>
       </div>
     </div>
   </div>
@@ -25,27 +25,25 @@ $usuario = $sesion->getUsuario();
 
 <div class="modal fade" id="modal_editar" tabindex="-1" aria-hidden="true">
   <div class="modal-dialog">
-    <div class="modal-content">
+    <div class="modal-content bg-dark text-light">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Editar Perfil</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <form name="form" action="Accion/editarPerfil.php" method="post">
+        <form name="form" class="needs-validation" action="Accion/editarPerfil.php" method="post" novalidate>
           <div class="col-lg-7 col-12">Username:
-            <input value='<?php echo $usuario->getUsnombre() ?>' type="text" style="width: 150px;"  name="usnombre"></input>
+            <input value='<?php echo $usuario->getUsnombre() ?>' class="form-control" type="text" style="width: 150px;"  name="usnombre" required></input>
           </div>
           <div class="col-lg-7 col-12">Mail:
-            <input value='<?php echo $usuario->getUsmail() ?>' type="text" style="width: 150px;"  name="usmail"></input>
+            <input value='<?php echo $usuario->getUsmail() ?>' class="form-control" type="text" style="width: 150px;"  name="usmail" required></input>
           </div>
           <div class="col-lg-7 col-12">Contraseña:
-            <input value='<?php echo $usuario->getUspass() ?>' type="password" style="width: 150px;" name="uspass"></input>
+            <input value='<?php echo $usuario->getUspass() ?>' class="form-control" type="password" style="width: 150px;" name="uspass" required></input>
           </div>
+          <button type="submit" class="btn editar text-light mt-2" id="botonModal" href="">Guardar</button>
+          <button type="button" class="btn btn-secondary mt-2" data-bs-dismiss="modal">Volver</button>
         </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Volver</button>
-        <button type="button" class="btn editar text-light" id="botonModal" href="">Guardar</button>
       </div>
     </div>
   </div>
@@ -56,7 +54,7 @@ $usuario = $sesion->getUsuario();
     <div class="modal-content bg-dark text-light">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Borrar cuenta</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <button type="button mt-2" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <h4>¿Esta seguro? Esta accion no puede deshacerse.</h4>
@@ -68,7 +66,9 @@ $usuario = $sesion->getUsuario();
     </div>
   </div>
 </div>
+<script src="Assets/Js/Validacion.js"> </script>
 <script src="Assets/Js/EditarPerfil.js"></script>
+
 <?php
 include_once('../Vista/Common/Footer.php')
 ?>
