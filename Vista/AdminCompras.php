adc3c6fd979f81a5d6e4c8b83a86b4fd72c7ded4
MartinaRosales <martina.est.fi.uncoma.edu.ar>
2022-11-25 16:38:20 -0300

    arregle el estilo

--- Vista/AdminCompras.php
+++ Vista/AdminCompras.php
@@ -15,7 +15,7 @@ if ($sesion->esAdmin()) {
         $i = 0;
 ?>
 
-<div class="container bg-dark">
+<div class="container bg-dark mt-3 mb-3">
     <h3 class="text-light">Gestion Compras</h3>
     <div class="rounded p-3 mb-2 bg-dark text-white">
         <table class="table table-dark table-hover p-5">
@@ -56,7 +56,7 @@ if ($sesion->esAdmin()) {
 
                         <td>
 
-                            <button class="ms-3 text-decoration-none btn btn-outline-warning" data-bs-toggle="modal" data-bs-target="#modal_estado"> <?php if ($estado->getIdcompraestadotipo() <= 4) {
+                            <button class="ms-3 text-decoration-none btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#modal_estado"> <?php if ($estado->getIdcompraestadotipo() <= 4) {
                                                                                                                                                         ?>Cambiar estado<?php
                                                                                                                                                                     } ?> </button>
 
@@ -73,13 +73,13 @@ if ($sesion->esAdmin()) {
 </div>
 
 <div class="modal fade" id="modal_estado" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
+    <div class="modal-dialog ">
+        <div class="modal-content bg-dark text-light">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Cambiar estado compra</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close btn-outline-light" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
+            <div class="modal-body text-light">
                 <h4>Elija el estado: </h4>
                 <form name="form" action="Accion/cambiarEstado.php" method="post">
 
@@ -102,10 +102,9 @@ if ($sesion->esAdmin()) {
                         }
                         ?>
                     </select>
-                    <button class="btn btn-outline-dark cambiarEstado" type="submit">Guardar</button>
+                    <button class="btn btn-outline-dark cambiarEstado text-light mt-2" type="submit" id="botonModal">Guardar</button>
+                    <button type="button" class="btn btn-secondary mt-2" data-bs-dismiss="modal">Volver</button>
                 </form>
-
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Volver</button>
 
             </div>
         </div>
