aee7eac2cbf23f05ecb9d1e07cfafa045fb240f6
MartinaRosales <martina.est.fi.uncoma.edu.ar>
2022-11-25 00:24:50 -0300

    cambie estilos

--- Vista/Common/Menu.php
+++ Vista/Common/Menu.php
@@ -8,14 +8,14 @@ if ($sesion->activa()) {
     $menues = $objMenuRol->menuesByIdRol($idRoles);
     $objMenu = new C_Menu();
     
-    $htmlMenu = $objMenu->armarMenu($menues) . "<div class='col align-self-end'><a href='Perfil.php'><button class='btn text-light mx-3'id='botonUser'>Perfil</button></a><a href='cerrarSesion.php'><button class='btn text-light' id='botonUser'>Cerrar Sesi&oacute;n</button></a></div>";
+    $htmlMenu = $objMenu->armarMenu($menues) . "<div class='col align-self-end'><a href='Perfil.php'><button class='btn text-light mx-3' id='botonUser'>Perfil</button></a><a href='cerrarSesion.php'><button class='btn text-light' id='botonUser'>Cerrar Sesi&oacute;n</button></a></div>";
     
 } else {
   //  $aux = "<div class='col align-self-end'><button class='btn text-light btn-outline-light' id='botonLogin' >Iniciar sesion</button></div>";
     $aux = "
     <ul class='nav nav-pills'>
-    <li class='nav-item dropdown'>
-                <a class='nav-link dropdown-toggle text-light fs-5' href='#' role='button' data-bs-toggle='dropdown' aria-expanded='false'>
+    <li class='nav-item dropdown' id='dropdownMenu'>
+                <a class='nav-link dropdown-toggle text-light fs-5' href='#' role='button' data-bs-toggle='dropdown' aria-expanded='false' id='dropdownMenu'>
                     Productos
                 </a>
             <ul class='dropdown-menu' id='dropdown'>
@@ -25,7 +25,7 @@ if ($sesion->activa()) {
             </ul>
             </li>
         <li class='nav-item'>
-            <a class='nav-link active text-light fs-5' aria-current='page' href='/TP-Final-Dinamica/Vista/Login.php'>Iniciar Sesion</a>
+            <a class='nav-link active text-light fs-5' aria-current='page' href='Login.php' id='botonUser'>Iniciar Sesi&oacute;n</a>
         </li>
         </ul>
     ";
