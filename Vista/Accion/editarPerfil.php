b666df40cd3d4435a834fede007e34938a2b621a
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2022-11-24 23:47:26 -0300

    no se que hacer con mi vida

--- Vista/Accion/editarPerfil.php
+++ Vista/Accion/editarPerfil.php
@@ -2,6 +2,9 @@
 include_once("../../configuracion.php");
 
 $datos = data_submitted();
-$objUsuario=new C_Usuario();
-$usuarioModificado=$objUsuario->modificacion($datos);
+
+$datos['uspass'] = md5($datos['uspass']);
+
+$objUsuario = new C_Usuario();
+$usuarioModificado = $objUsuario->modificacion($datos);
 ?>
