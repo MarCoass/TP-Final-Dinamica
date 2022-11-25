1184cec6324d6b138b6667a2e75450ad98079226
MartinaRosales <martina.est.fi.uncoma.edu.ar>
2022-11-25 10:23:46 -0300

    estilo del carrito

--- Vista/Common/Footer.php
+++ Vista/Common/Footer.php
@@ -10,7 +10,7 @@ $carrito = $sesion->obtener_carrito();
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-   <table class="table table-striped">
+   <table class="table table-striped text-light">
   <thead>
     <tr>
       <th scope="col">#</th>
