62a03c3f1b5d5ce1158c913096b3e3c55c1962c8
patito <micaela.paillamilla@est.fi.uncoma.edu.ar>
2022-11-26 18:25:35 -0300

    correciones adminUsuario

--- Vista/AdminUsuarios.php
+++ Vista/AdminUsuarios.php
@@ -2,31 +2,31 @@
 /**este es usando data table */
 include_once('Common/Header.php');
 ?>
-
+<script src="Assets/Js/AdminUsuarios.js"></script>
 <div class="container-fluid bg-light">
 
 <table id="lista_de_productos" class="table table-bordered nowrap table-striped table-condensed" style="width:100%">
-                                                        <thead>
-                                                        <tr>
-                                                            <th data-priority="1"></th>	
-                                                            <th class="text" data-priority="1"> Nombre </th>									
-                                                            <th class="text" data-priority="1"> Mail </th>
-                                                            <th class="text" data-priority="1"> Rol </th>
-                                                            <th class="text" data-priority="1"> Estado </th>
-                                                        </tr>
-                                                        </thead>
-                                                        <tbody>    
-                                                        </tbody>
-                                                        <tfoot>
-                                                        <tr> 
-                                                        <th ></th>	
-                                                            <th > Nombre </th>									
-                                                            <th > Mail </th>
-                                                            <th > Rol </th>
-                                                            <th > Estado </th>                                         	  	                                       	                                          	                                          	  	                 	  	                                      	  
-                                                        </tr>
-                                                        </tfoot>      
-                                                    </table>
+    <thead>
+        <tr>
+            <th data-priority="1"></th>	
+            <th class="text" data-priority="1"> Nombre </th>									
+            <th class="text" data-priority="1"> Mail </th>
+            <th class="text" data-priority="1"> Rol </th>
+            <th class="text" data-priority="1"> Estado </th>
+        </tr>
+    </thead>
+    <tbody>    
+    </tbody>
+    <tfoot>
+        <tr> 
+            <th></th>	
+            <th > Nombre </th>									
+            <th > Mail </th>
+            <th > Rol </th>
+            <th > Estado </th>                                         	  	                                       	                                          	                                          	  	                 	  	                                      	  
+        </tr>
+    </tfoot>      
+</table>
 
 </div>
 
@@ -34,5 +34,5 @@ include_once('Common/Header.php');
  <input type="hidden" name="idUsuario" id="idUsuario" />
 </form>
 
-<script src="Assets/Js/AdminUsuarios.js"></script>
+
 <?php include_once('Common/Footer.php'); ?>
--- Vista/Assets/Js/AdminUsuarios.js
+++ Vista/Assets/Js/AdminUsuarios.js
@@ -1,62 +1,52 @@
 $(document).ready(function () {
-  var lista_de_productos = $("#lista_de_productos").DataTable({
+ $('#lista_de_productos').DataTable( {			
     ajax: {
-      type: "POST",
-      url: "Accion/obtenerUsuarios.php",
-      dataSrc: "",
-      data: function (d) {
-        d.id_cliente = $("#id_cliente").val();
-        d.id_producto = $("#id_producto").val();
-        d.id_sucursal = $("#sucursal option:selected").val();
-      },
+        type: "POST",
+        url: "Accion/obtenerUsuarios.php",
+        dataSrc: '',
+        data: '',
     },
-    processing: true,
+    processing : true,
     responsive: true,
-    language: {
-      decimal: ",",
-      thousands: ".",
-      search: "Buscar: ",
-      processing: "Obteniendo datos...",
-      lengthMenu: "Mostrar MENU elementos por página",
-      zeroRecords: "Sin resultados",
-      info: "Mostrando PAGE de PAGES páginas",
-      infoEmpty: "No se encontraron elementos",
-      infoFiltered: "(filtrado de MAX total elementos)",
-      paginate: {
-        first: "Primera",
-        last: "Última",
-        next: "Siguiente",
-        previous: "Anterior",
-      },
+    "language": {
+        "decimal": ",",
+        "thousands": ".",
+        "search": "Buscar: ",
+        "processing": "Obteniendo datos...",
+        "lengthMenu": "Mostrar MENU elementos por página",
+        "zeroRecords": "Sin resultados",
+        "info": "Mostrando PAGE de PAGES páginas",
+        "infoEmpty": "No se encontraron elementos",
+        "infoFiltered": "(filtrado de MAX total elementos)",
+        "paginate": {
+            "first": "Primera",
+            "last": "Última",
+            "next": "Siguiente",
+            "previous": "Anterior"
+        }
     },
-    lengthMenu: [
-      [10, 50, -1],
-      [10, 50, "Todos"],
-    ],
-    dom: "frtipB",
+    "lengthMenu": [[10,50,-1], [10, 50, "Todos"]],
+    dom: 'frtipB',
     buttons: [
-      {
-        extend: "pageLength",
-        text: '<i class="fa fa-eye"></i> Elementos',
-        className: "buttons-excel buttons-html5 btn red btn-outline",
-      },
-      {
-        extend: "excelHtml5",
-        text: '<i class="fa fa-file-excel-o"></i> Excel',
-        className: "buttons-excel buttons-html5 btn red btn-outline",
-        title: "Exportar_excel",
-      },
+              {
+                  extend: 'pageLength',
+                  text:      '<i class="fa fa-eye"></i> Elementos',
+                  className: 'buttons-excel buttons-html5 btn red btn-outline',                      
+                  
+              },     
+              {
+                  extend: 'excelHtml5',
+                  text:      '<i class="fa fa-file-excel-o"></i> Excel',
+                  className: 'buttons-excel buttons-html5 btn red btn-outline',
+                  title: 'Exportar_excel'
+              }                                             
     ],
-    columnDefs: [
-      {
-        targets: 0,
-        checkboxes: {
-          selectRow: true,
-        },
-      },
-    ],
-    select: {
-      style: "multi",
-    },
-  });
+    columnDefs: [ 	             
+          {   'targets': 0,
+              'checkboxes': {
+                 'selectRow': true
+              }
+          }                   
+    ],       
+  } );
 });
