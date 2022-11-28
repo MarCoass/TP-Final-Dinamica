8ba831de771a132156b7551f7c92547b835bcb08
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2022-11-28 15:40:05 -0300

    saque el menu del controlador

--- Control/C_Menu.php
+++ Control/C_Menu.php
@@ -141,54 +141,7 @@ class C_Menu
         return $arreglo;
     }
 
-    //AIUDA
+    
 
-    /**Funcion que arma la estructura de un menu (esto puede malir sal)
-     * Recibe por parameto un array de menues (obtenidos con menuesByIdRol() de C_Menurol)
-     * tiene foreachs anidados asi que debe ser la cosa menos eficiente jamas vista, pero solo me salio asi si es para un menu de hasta 2 niveles
-     * **/
-    public function armarMenu($menues)
-    {
-        
-        $htmlCompleto = '';
-        foreach ($menues as $itemMenu) {
-            $htmlHijos = [];
-            if ($itemMenu->getIdpadre() == NULL) { // Si no tiene padre crea el li y revisa si tiene hijos
-
-                //Recorro los menus y busco los hijos de este menu
-                foreach ($menues as $menu) {
-                    //Si el menu tiene idpadre igual al id del menu actual, lo agrego a un array
-                    if ($menu->getIdpadre() == $itemMenu->getIdmenu()) {
-                        //$hijos[] = $itemMenu;
-                        $htmlHijos[] = "<li><a class='dropdown-item text-light' href=".$menu->getScript().">{$menu->getMenombre()}</a></li>";
-                        //echo "<li><a class='dropdown-item text-dark' href=".$menu->getScript().">{$menu->getMenombre()}</a></li>";
-                    }
-                }
-                if (count($htmlHijos) > 0) {
-                    
-                    $htmlItemMenu = "<li class='nav-item dropdown'><a class= 'nav-link dropdown-toggle text-light fs-5' data-bs-toggle='dropdown' href='#' role='button' aria-expanded='false' id='dropdownMenu'>{$itemMenu->getMenombre()}</a>";
-                    
-                    $htmlDesplegable = "<ul class='dropdown-menu'id='dropdownMenu'> ";//aca va el id='dropdownMenu'
-                    foreach ($htmlHijos as $item) {
-                        $htmlDesplegable = $htmlDesplegable . $item;
-                    }
-                    $htmlDesplegable = $htmlDesplegable . "</ul>";
-                    $htmlItemMenu = $htmlItemMenu . $htmlDesplegable . "</li>";
-                } else {
-                    $htmlItemMenu = "<li class='nav-item'><a class='nav-link text-light' href='#'>{$itemMenu->getMenombre()}</a></li>";
-                }
-                
-                $arrayItemsMenu[] = $htmlItemMenu;
-            } else {
-                //NO HACE NADA, ENTONCES SI ES UN ITEM HIJO ES IGNORADO HASTA QUE ENCUENTRE A SU PADRE
-            }
-        }
-        $htmlCompleto = "<ul class='nav nav-pills text-light' id='navbarHeader'>";
-        foreach ($arrayItemsMenu as $item) {
-            $htmlCompleto = $htmlCompleto . $item;
-        }
-        $htmlCompleto = $htmlCompleto ."</ul>";
-        //echo $htmlCompleto;
-        return $htmlCompleto;
-    }
+    
 }
--- Vista/Common/Menu.php
+++ Vista/Common/Menu.php
@@ -1,17 +1,64 @@
 <?php
+
 //echo $_SESSION['ROOT'].'Modelo/';
 $menues = [];
-
 if ($sesion->activa()) {
     $idRoles = $sesion->getRoles();
     $objMenuRol = new C_Menurol();
     $menues = $objMenuRol->menuesByIdRol($idRoles);
     $objMenu = new C_Menu();
-    
-    $htmlMenu = $objMenu->armarMenu($menues) . "<div class='col align-self-end'><a href='Perfil.php'><button class='btn text-light mx-3 fs-5'>Perfil</button></a><a href='cerrarSesion.php'><button class='btn text-light fs-5'>Cerrar Sesi&oacute;n</button></a></div>";
-    
+
+
+    $htmlCompleto = '';
+    foreach ($menues as $itemMenu) {
+        $htmlHijos = [];
+        if ($itemMenu->getIdpadre() == NULL) { // Si no tiene padre crea el li y revisa si tiene hijos
+
+            //Recorro los menus y busco los hijos de este menu
+            foreach ($menues as $menu) {
+                //Si el menu tiene idpadre igual al id del menu actual, lo agrego a un array
+                if ($menu->getIdpadre() == $itemMenu->getIdmenu()) {
+                    //$hijos[] = $itemMenu;
+                    $htmlHijos[] = "<li><a class='dropdown-item text-light' href=" . $menu->getScript() . ">{$menu->getMenombre()}</a></li>";
+                    //echo "<li><a class='dropdown-item text-dark' href=".$menu->getScript().">{$menu->getMenombre()}</a></li>";
+                }
+            }
+            if (count($htmlHijos) > 0) {
+
+                $htmlItemMenu = "<li class='nav-item dropdown'><a class= 'nav-link dropdown-toggle text-light fs-5' data-bs-toggle='dropdown' href='#' role='button' aria-expanded='false' id='dropdownMenu'>{$itemMenu->getMenombre()}</a>";
+
+                $htmlDesplegable = "<ul class='dropdown-menu'id='dropdownMenu'> "; //aca va el id='dropdownMenu'
+                foreach ($htmlHijos as $item) {
+                    $htmlDesplegable = $htmlDesplegable . $item;
+                }
+                $htmlDesplegable = $htmlDesplegable . "</ul>";
+                $htmlItemMenu = $htmlItemMenu . $htmlDesplegable . "</li>";
+            } else {
+                $htmlItemMenu = "<li class='nav-item'><a class='nav-link text-light' href='#'>{$itemMenu->getMenombre()}</a></li>";
+            }
+
+            $arrayItemsMenu[] = $htmlItemMenu;
+        } else {
+            //NO HACE NADA, ENTONCES SI ES UN ITEM HIJO ES IGNORADO HASTA QUE ENCUENTRE A SU PADRE
+        }
+    }
+    $htmlCompleto = "<ul class='nav nav-pills text-light' id='navbarHeader'>";
+    foreach ($arrayItemsMenu as $item) {
+        $htmlCompleto = $htmlCompleto . $item;
+    }
+    $htmlCompleto = $htmlCompleto . "</ul>";
+
+
+
+
+
+
+
+
+
+    $htmlMenu = $htmlCompleto . "<div class='col align-self-end'><a href='Perfil.php'><button class='btn text-light mx-3 fs-5'>Perfil</button></a><a href='cerrarSesion.php'><button class='btn text-light fs-5'>Cerrar Sesi&oacute;n</button></a></div>";
 } else {
-  //  $aux = "<div class='col align-self-end'><button class='btn text-light btn-outline-light' id='botonLogin' >Iniciar sesion</button></div>";
+    //  $aux = "<div class='col align-self-end'><button class='btn text-light btn-outline-light' id='botonLogin' >Iniciar sesion</button></div>";
     $aux = "
     <ul class='nav nav-pills'>
     <li class='nav-item dropdown'>
@@ -29,7 +76,7 @@ if ($sesion->activa()) {
         </li>
         </ul>
     ";
-    
+
     $htmlMenu = $aux;
 }
 ?>
