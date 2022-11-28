ff998aa46f8c37b68ab05ab035cfb1a0cbb761ec
patito <micaela.paillamilla@est.fi.uncoma.edu.ar>
2022-11-27 21:39:13 -0300

    implementacion de mis compras en cliente finalizada

--- Vista/Accion/cambiarEstado.php
+++ Vista/Accion/cambiarEstado.php
@@ -3,18 +3,97 @@
 include_once("../Common/Header.php");
 
 $datos = data_submitted();
-$objCompraEstado = new C_Compraestado();
+$obj_estado = new C_Compraestado();
+$obj_compra = new C_Compra();
+$obj_compra_item = new C_Compraitem();
+//falta verificar a que tipo de estado se quiere transferir
+// si es de tipo 4 osea cancelar solo debe permitirlo si la compara tiene
+// estado uno sino no
 
-$fecha = new DateTime();
-$fechaStamp = $fecha->format('Y-m-d H:i:s');
+//si se desea cancelar una compra de estado uno
+//se debe registrar el estado aterior y registrar el nuevo estado de la compra
+//luego de eso el stock de la compra vuelve a los productos de la pagina
 
-$compraEstado = $objCompraEstado->buscar($datos);
-$paramCompraEstadoNuevo = [
-    "idcompraestado" => $datos["idcompraestado"],
-    "idcompra" => $datos["idcompra"],
-    "idcompraestadotipo" => $datos["idcompraestadotipo"],
-    "cefechaini" => $fechaStamp,
-    "cefechafin" => null,
-];
 
-$objCompraEstado->modificacion($paramCompraEstadoNuevo);
+$compra = $obj_compra->buscar(array('idcompra'=> $datos["idcompra"]));
+
+if($compra != null){
+    $estado = $obj_estado->buscar(array('idcompra' =>  $datos["idcompra"] ));
+    if($datos["idcompraestadotipo"] == 4){
+        $param_estado_anterior = array(
+            'idcompraestado' => NULL,
+            'idcompra' =>   $datos["idcompra"],
+            'idcompraestadotipo' => 1,
+            'cefechaini' => $estado[0]->getCefechaini(),
+            'cefechafin' => date('Y-m-d H:i:s')
+        );
+        
+        $obj_estado->alta($param_estado_anterior);
+
+        $param_estado_nuevo = array(
+            'idcompraestado' => $estado[0]->getIdcompraestado(),
+            'idcompra' => $datos["idcompra"],
+            'idcompraestadotipo' => $datos["idcompraestadotipo"] ,
+            'cefechaini' => $estado[0]->getCefechaini(),
+            'cefechafin' => NULL
+        );
+
+        
+        $obj_estado->modificacion($param_estado_nuevo);
+
+     
+        $productos_compra = $obj_compra_item->buscar(array('idcompra' =>$datos["idcompra"]));
+    
+        foreach($productos_compra as $indice => $prd){
+        
+                $producto = $prd->getIdproducto();
+              
+                $producto->setProcantstock($producto->getProcantstock()+$prd->getCicantidad());
+               
+                //modifico el stock
+                $producto->modificar();
+        }
+    }
+
+    if($datos["idcompraestadotipo"] == 1){
+            $param_estado_anterior = array(
+                'idcompraestado' => NULL,
+                'idcompra' =>   $datos["idcompra"],
+                'idcompraestadotipo' => 4,
+                'cefechaini' => $estado[0]->getCefechaini(),
+                'cefechafin' => date('Y-m-d H:i:s')
+            );
+            
+            $obj_estado->alta($param_estado_anterior);
+    
+            $param_estado_nuevo = array(
+                'idcompraestado' => $estado[0]->getIdcompraestado(),
+                'idcompra' => $datos["idcompra"],
+                'idcompraestadotipo' => $datos["idcompraestadotipo"] ,
+                'cefechaini' => $estado[0]->getCefechaini(),
+                'cefechafin' => NULL
+            );
+    
+            
+            $obj_estado->modificacion($param_estado_nuevo);
+    
+         
+            $productos_compra = $obj_compra_item->buscar(array('idcompra' => $datos["idcompra"]));
+        
+            foreach($productos_compra as $indice => $prd){
+            
+                    $producto = $prd->getIdproducto();
+                  
+                    $producto->setProcantstock($producto->getProcantstock()-$prd->getCicantidad());
+                   
+                    //modifico el stock
+                    $producto->modificar();
+            }
+        }
+   
+}
+?>
+<script>
+window.history.back();
+</script>
+    
--- Vista/MisCompras.php
+++ Vista/MisCompras.php
@@ -11,14 +11,14 @@ if(is_array($arrayCompras) && count($arrayCompras)>0){
     }else{
     $cantidadCompras = 0;
     }
-$arrayCompras = $objCompra->buscar(['idusuario' => $usuario->getIdusuario()]);
-$cantidadCompras = count($arrayCompras);
+//$arrayCompras = $objCompra->buscar(['idusuario' => $usuario->getIdusuario()]);
+//$cantidadCompras = count($arrayCompras);
 ?>
 
 <div class="container bg-dark mt-3 mb-3">
 <h3 class="text-light">Mis Compras</h3>
     <div class="rounded p-3 mb-2 bg-dark text-white">
-        <table class="table table-dark table-hover p-5">
+        <table id="mis_compras" class="table table-dark table-hover p-5">
             <thead class="text-center">
                 <tr>
                     <th scope="col-4">N°</th>
@@ -31,6 +31,7 @@ $cantidadCompras = count($arrayCompras);
             </thead>
             <tbody>
                 <?php
+                if($cantidadCompras != 0){
                 while ($i < $cantidadCompras) {
                     //obtengo el estado
                     $objCompraEstado = new C_Compraestado();
@@ -39,13 +40,16 @@ $cantidadCompras = count($arrayCompras);
                     $idcompra = $arrayCompras[$i]->getIdcompra();
                     //Obtengo los productos
                     $objCompraItem = new C_Compraitem();
-                    $arrayProductos = $objCompraItem->traerProductos($arrayCompras[$i]->getIdcompra());
-                ?>
+                    //$arrayProductos = $objCompraItem->traerProductos($arrayCompras[$i]->getIdcompra());
+                    $productos_compra = $objCompraItem->buscar(array('idcompra' => $arrayCompras[$i]->getIdcompra()));
+
+                ?>  
                     <tr>
                         <th scope="row" class="text-center"><?php echo $i + 1 ?></th>
-                        <td> <?php echo $arrayCompras[$i]->getCofecha() ?> </td>
-                        <td> <?php foreach ($arrayProductos as $producto) {
-                                    echo $producto->getPronombre(); ?> <br> <?php
+                        <td> <?php echo date('d/m/Y H:i:s', strtotime($arrayCompras[$i]->getCofecha())) ?> </td>
+                        <td> <?php foreach ($productos_compra as $prd) {
+                                $producto = $prd->getIdproducto();
+                                echo '<span title="Cantidad retirada: '.$prd->getCicantidad().'">'.$producto->getPronombre().'</span>'; ?> <br> <?php
                                                                 }
                                                                     ?> </td>
                         <td> <?php echo $objCompraItem->totalCompra($arrayCompras[$i]->getIdcompra()) ?>
@@ -54,23 +58,80 @@ $cantidadCompras = count($arrayCompras);
                             <?php echo $estado->getCetdescripcion() ?>
                         </td>
                         <td>
-                            <form name="form" action="Accion/cambiarEstado.php" method="post">
-                                <input style="display:none;" name='idcompraestado' id='idcompraestado' value='<?php echo $objCompraEstado->getIdcompraestado();  ?>'>
-                                <input style="display:none;" name='idcompra' id='idcompra' value='<?php echo $idcompra ?>'>
-                                <input style="display:none;" name='idcompraestadotipo' id='idcompraestadotipo' value='4'>
-                                <button class="btn cambiarEstado btn-outline-light" type="submit" <?php if ($estado->getIdcompraestadotipo() != 1) { ?> disabled <?php  } ?>>Cancelar</button>
-                            </form>
 
+                        <?php if($estado->getIdcompraestadotipo() == 1){ ?>
+                            <form name="form" action="Accion/cambiarEstado.php" method="post">
+                                <input type="hidden" name='idcompraestado' id='idcompraestado' value='<?php echo $objCompraEstado->getIdcompraestado();  ?>'>
+                                <input type="hidden" name='idcompra' id='idcompra' value='<?php echo $idcompra ?>'>
+                                <input type="hidden" name='idcompraestadotipo' id='idcompraestadotipo' value='4'>
+                                <button class="btn btn-outline-light" type="submit" <?php if ($estado->getIdcompraestadotipo() != 1) { ?> disabled <?php  } ?>>Cancelar</button>
+                            </form>
+                        <?php } ?>
+
+                        <?php if($estado->getIdcompraestadotipo() == 4){ ?>
+                            <form name="form_iniciar_compra" action="Accion/cambiarEstado.php" method="post">
+                                <input type="hidden" name='idcompraestado' id='idcompraestado' value='<?php echo $objCompraEstado->getIdcompraestado();  ?>'>
+                                <input type="hidden" name='idcompra' id='idcompra' value='<?php echo $idcompra ?>'>
+                                <input type="hidden" name='idcompraestadotipo' id='idcompraestadotipo' value='1'>
+                                <button class="btn btn-outline-light" type="submit">Iniciar Compra</button>
+                            </form>
+                        <?php } ?>
                         </td>
                     </tr>
                 <?php
                     $i++;
-                } ?>
+                }} ?>
             </tbody>
         </table>
     </div>
 </div>
-<script src="Assets/Js/cambiarEstadoCompra.js"></script>
+<script>
+$(document).ready(function () {
+ $('#mis_compras').DataTable( {			
+    processing : true,
+    responsive: true,
+    "language": {
+        "decimal": ",",
+        "thousands": ".",
+        "search": "Buscar: ",
+        "processing": "Obteniendo datos...",
+        "lengthMenu": "Mostrar MENU elementos por página",
+        "zeroRecords": "Sin resultados",
+        "info": "Mostrando PAGE de PAGES páginas",
+        "infoEmpty": "No se encontraron elementos",
+        "infoFiltered": "(filtrado de MAX total elementos)",
+        "paginate": {
+            "first": "Primera",
+            "last": "Última",
+            "next": "Siguiente",
+            "previous": "Anterior"
+        }
+    },
+    "lengthMenu": [[10,50,-1], [10, 50, "Todos"]],
+    dom: 'frtipB',
+    buttons: [
+              {
+                  extend: 'pageLength',
+                  text:      '<i class="fa fa-eye"></i> Elementos',
+                  className: 'buttons-excel buttons-html5 btn red btn-outline',                      
+                  
+              },     
+              {
+                  extend: 'excelHtml5',
+                  text:      '<i class="fa fa-file-excel-o"></i> Excel',
+                  className: 'buttons-excel buttons-html5 btn red btn-outline',
+                  title: 'Exportar_excel'
+              }                                             
+    ],
+    columnDefs: [ 	             
+          {   'targets': 0,
+            
+          }                   
+    ],       
+  } );
+});
+
+</script>
 <?php
 include_once("Common/Footer.php");
 ?>
