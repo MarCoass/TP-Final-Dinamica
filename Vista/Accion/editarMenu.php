63b866997b8da5eb4e13fce6df4364bf3d2e18be
MartinaRosales <martina.rosales@est.fi.uncoma.edu.ar>
2022-11-28 20:11:07 -0300

    avance con editar menu (no terminado)

--- Vista/Accion/editarMenu.php
+++ Vista/Accion/editarMenu.php
@@ -4,5 +4,6 @@ $datos=data_submitted();
 $objMenu=new C_Menu();
 $menuModificado=$objMenu->modificacion($datos);
 
-
+$objMenuRol = new C_Menurol
+$menuRol = $objMenuRol->buscar()
 $rolAgregado=$objMenu->modificarRoles($datos);
