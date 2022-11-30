a25746063107aa08f9a9ad0981db07cd387c67ea
patito <micaela.paillamilla@est.fi.uncoma.edu.ar>
2022-11-30 00:17:15 -0300

    mejoras en registrar usuario

--- Vista/registrar_usuario.php
+++ Vista/registrar_usuario.php
@@ -1,32 +1,28 @@
 <?php
 include_once("Common/Header.php");
-
-include_once '../Control/AbmUsuario.php';
-include_once '../Modelo/Usuario.php';
-include_once '../Modelo/UsuarioRol.php';
-include_once '../Modelo/UsuarioRol.php';
 include_once 'Login.php';
 
 $datos = data_submitted();
 $name = $datos['usuario_nuevo'];
 $pass = md5($datos['password_nuevo']);
 $mail = $datos['mail_nuevo'];
-$sesion->iniciar($name, $pass);
-$obj_Usuario = new Usuario();
-$obj_Usuario->setear(null, $name, $pass, $mail,NULL);
-$obj_Usuario->insertar();
 
+
+$obj_Usuario = new C_Usuario();
 $obj_AbmUsuario = new AbmUsuario();
+$existe_usuario = $obj_AbmUsuario->buscar(array('usnombre' => $name,'usmail' => $mail));
+
+if(count($existe_usuario) == 0){
+
+$obj_Usuario->alta(array('usnombre' => $name,'uspass' => $pass,'usmail'=>$mail));
+
 $param = array();
 $param['usnombre'] = $name;
 $param['uspass'] = $pass;
 $param['usmail'] = $mail;
 $usuario = $obj_AbmUsuario->buscar($param);
 
-$obj_UsuarioRol = new UsuarioRol();
-$obj_UsuarioRol->cargar(null, $usuario[0]->getidusuario(), '3');
-$obj_UsuarioRol->insertar();
-
+$obj_UsuarioRol = new C_Usuariorol();
+$obj_UsuarioRol->alta(array('idusuario' => $usuario[0]->getidusuario(), 'idrol' => 3));
+}
 ?>
-
-
